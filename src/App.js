be93268e877f3b6c16afe9dd2760f387201b87f8
equinoxfires <evan@brinkbit.com>
2018-06-18 22:19:23 -0400

    Replace video background with photo background

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { Info } from './Info.js';
 import { CampingInfo } from './CampingInfo.js';
 import { ReserveCamping } from './ReserveCamping.js';
 import { ReserveNoCamping } from './ReserveNoCamping.js';
+
 import logo from './media/wedfest_logo.png';
 import names from './media/wedfest_names_header.png';
+import header from './media/trees_header.jpg';
+
 import './App.css';
+
 import {
     BrowserRouter as Router,
     Route,
@@ -24,8 +28,9 @@ class App extends Component {
 
     return (
       <div className="App">
-          <iframe id="video-background" frameBorder="0" src="https://www.youtube.com/embed/xNN7iTA57jM?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1&modestbranding=1" allow="autoplay; encrypted-media; modest-branding" allowFullScreen>
-          </iframe>
+          {/* <iframe id="video-background" frameBorder="0" src="https://www.youtube.com/embed/xNN7iTA57jM?rel=0&amp;controls=0&amp;showinfo=0;autoplay=1&modestbranding=1" allow="autoplay; encrypted-media; modest-branding" allowFullScreen>
+          </iframe> */}
+          <div id="photo-background" />
 
           <img src={logo} id="wedfest-logo" alt="logo" />
           <img src={names} id="names-header" alt="evan & amanda" />
